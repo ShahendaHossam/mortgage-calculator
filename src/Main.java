961c9964546424b3b7a07bb6ccb6fd75cee01147
Shahenda <shahenda.cs.99@gmail.com>
2023-05-31 15:33:51 +0200

    add some conditions where if the user enter a principle value if the condition/value is false a message appear to the user to enter another correct value the same as annual interest rate and period

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,55 @@ public class Main {
     public static void main(String[] args) {
         final byte PERCENT = 100;
         final byte MONTHS_IN_YEAR = 12;
+
+        double annual_interest_rate = 0;
         Scanner scanner = new Scanner(System.in);
 
+        //first print in the project
         System.out.print("Principal: ");
         int principal = scanner.nextInt();
-
+        //if the condition in the while loop is false a msg for entering a proper value will be displayed over and over until the user enter the correct value
+        //if the condition is true in the if condition it will break the code and will not display its value in the terminal but will store it
+        while(!(principal >= 1_000 && principal <= 1_000_000)){
+            System.out.println("Enter a number between 1,000 and 1,000,000 ");
+            System.out.print("Principal: ");
+             principal = scanner.nextInt();
+            if(principal >= 1_000 && principal <= 1_000_000){
+                break;
+            }
+        }
         System.out.print("Annual Interest Rate: ");
         double interest_rate = scanner.nextDouble();
-        double interest_rate_percent = interest_rate / PERCENT;
-        double annual_interest_rate = interest_rate_percent / MONTHS_IN_YEAR;
-
+        while(!(interest_rate > 0 && interest_rate <= 30)){
+            System.out.println("Enter a value greater than 0 and less than or equal 30 ");
+            System.out.print("Annual Interest Rate: ");
+            interest_rate = scanner.nextDouble();
+            if(interest_rate > 0 && interest_rate <= 30){
+                double interest_rate_percent = interest_rate / PERCENT;
+                annual_interest_rate = interest_rate_percent / MONTHS_IN_YEAR;
+                break;
+            }
+        }
         System.out.print("Period(Years): ");
         int period = scanner.nextShort();
-        int year_period = period * MONTHS_IN_YEAR;
+        while(!(period >= 1 && period <= 30)){
+            System.out.println("Enter a value between 1 and 30");
+            System.out.print("Period(Years): ");
+            period = scanner.nextShort();
+            if(period >= 1 && period <= 30){
+                int year_period = period * MONTHS_IN_YEAR;
 
-        double mortgage_calculator = principal * (annual_interest_rate * Math.pow(1 + annual_interest_rate , year_period)) / (Math.pow(1 + annual_interest_rate , year_period) - 1);
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String result = currency.format(mortgage_calculator);
-        System.out.println(result);
+                double mortgage_calculator = principal * (annual_interest_rate * Math.pow(1 + annual_interest_rate , year_period)) / (Math.pow(1 + annual_interest_rate , year_period) - 1);
+                NumberFormat currency = NumberFormat.getCurrencyInstance();
+                String result = currency.format(mortgage_calculator);
+                System.out.println(result);
+            }
+        }
+
+
+
+
+        //another solution:-
+
     }
 }
