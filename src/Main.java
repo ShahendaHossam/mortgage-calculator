441a596c4e52d5d62d1f013bb8d2c6c92e14e638
Shahenda <shahenda.cs.99@gmail.com>
2023-06-11 10:15:08 +0200

    create calculateMortgage function I created this function to write less code lines in the main function and then call this function in the main function to execute it

--- src/Main.java
+++ src/Main.java
@@ -5,101 +5,161 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        final byte PERCENT = 100;
-        final byte MONTHS_IN_YEAR = 12;
-
-        double annual_interest_rate ;
-
         int principal;
-        double interest_rate;
-        int period;
+        float annualInterest;
+        byte years;
         Scanner scanner = new Scanner(System.in);
 
-//        first print in the project
+        // first print in the project
 
-//        System.out.print("Principal: ");
-//        int principal = scanner.nextInt();
+        // System.out.print("Principal: ");
+        // int principal = scanner.nextInt();
 
-//        //if the condition in the while loop is false a msg for entering a proper value will be displayed over and over until the user enter the correct value
-//        //if the condition is true in the if condition it will break the code and will not display its value in the terminal but will store it
-//        while(!(principal >= 1_000 && principal <= 1_000_000)){
-//            System.out.println("Enter a number between 1,000 and 1,000,000 ");
-//            System.out.print("Principal: ");
-//             principal = scanner.nextInt();
-//            if(principal >= 1_000 && principal <= 1_000_000){
-//                break;
-//            }
-//        }
-//        System.out.print("Annual Interest Rate: ");
-//        double interest_rate = scanner.nextDouble();
-//        while(!(interest_rate > 0 && interest_rate <= 30)){
-//            System.out.println("Enter a value greater than 0 and less than or equal 30 ");
-//            System.out.print("Annual Interest Rate: ");
-//            interest_rate = scanner.nextDouble();
-//            if(interest_rate > 0 && interest_rate <= 30){
-//                double interest_rate_percent = interest_rate / PERCENT;
-//                annual_interest_rate = interest_rate_percent / MONTHS_IN_YEAR;
-//                break;
-//            }
-//        }
-//        System.out.print("Period(Years): ");
-//        int period = scanner.nextShort();
-//        while(!(period >= 1 && period <= 30)){
-//            System.out.println("Enter a value between 1 and 30");
-//            System.out.print("Period(Years): ");
-//            period = scanner.nextShort();
-//            if(period >= 1 && period <= 30){
-//                int year_period = period * MONTHS_IN_YEAR;
-//
-//                double mortgage_calculator = principal * (annual_interest_rate * Math.pow(1 + annual_interest_rate , year_period)) / (Math.pow(1 + annual_interest_rate , year_period) - 1);
-//                NumberFormat currency = NumberFormat.getCurrencyInstance();
-//                String result = currency.format(mortgage_calculator);
-//                System.out.println(result);
-//            }
-//        }
+        // //if the condition in the while loop is false a msg for entering a proper
+        // value will be displayed over and over until the user enter the correct value
+        // //if the condition is true in the if condition it will break the code and
+        // will not display its value in the terminal but will store it
+        // while(!(principal >= 1_000 && principal <= 1_000_000)){
+        // System.out.println("Enter a number between 1,000 and 1,000,000 ");
+        // System.out.print("Principal: ");
+        // principal = scanner.nextInt();
+        // if(principal >= 1_000 && principal <= 1_000_000){
+        // break;
+        // }
+        // }
+        // System.out.print("Annual Interest Rate: ");
+        // double interest_rate = scanner.nextDouble();
+        // while(!(interest_rate > 0 && interest_rate <= 30)){
+        // System.out.println("Enter a value greater than 0 and less than or equal 30
+        // ");
+        // System.out.print("Annual Interest Rate: ");
+        // interest_rate = scanner.nextDouble();
+        // if(interest_rate > 0 && interest_rate <= 30){
+        // double interest_rate_percent = interest_rate / PERCENT;
+        // annual_interest_rate = interest_rate_percent / MONTHS_IN_YEAR;
+        // break;
+        // }
+        // }
+        // System.out.print("Period(Years): ");
+        // int period = scanner.nextShort();
+        // while(!(period >= 1 && period <= 30)){
+        // System.out.println("Enter a value between 1 and 30");
+        // System.out.print("Period(Years): ");
+        // period = scanner.nextShort();
+        // if(period >= 1 && period <= 30){
+        // int year_period = period * MONTHS_IN_YEAR;
+        //
+        // double mortgage_calculator = principal * (annual_interest_rate * Math.pow(1 +
+        // annual_interest_rate , year_period)) / (Math.pow(1 + annual_interest_rate ,
+        // year_period) - 1);
+        // NumberFormat currency = NumberFormat.getCurrencyInstance();
+        // String result = currency.format(mortgage_calculator);
+        // System.out.println(result);
+        // }
+        // }
 
 
 
+        // another solution:-
+        // this solution is the right one because in the above code will make all the
+        // cases right except one case which is the user enters all the condition right
+        // from the first time
+        // but the above solution first the user must enter the wrong value and then the
+        // correct value to print the value and this is not correct
+        // he must in the while loop the condition be true.
+        // while (true) {
+        // System.out.print("Principal: ");
+        // principal = scanner.nextInt();
+        // if (principal >= 1_000 && principal <= 1_000_000) {
+        // break;
+        // }
+        // System.out.println("Enter a number between 1,000 and 1,000,000 ");
+        // }
 
-        //another solution:-
-        //this solution is the right one because in the above code will make all the cases right except one case which is the user enters all the condition right from the first time
-        //but the above solution first the user must enter the wrong value and then the correct value to print the value and this is not correct
-        //he must in the while loop the condition be true.
-        while(true){
+        // while (true) {
+        // System.out.print("Annual Interest Rate: ");
+        // interest_rate = scanner.nextDouble();
+        // if (interest_rate > 0 && interest_rate <= 30) {
+        // double interest_rate_percent = interest_rate / PERCENT;
+        // annual_interest_rate = interest_rate_percent / MONTHS_IN_YEAR;
+        // break;
+        // }
+        // System.out.println("Enter a value greater than 0 and less than or equal 30
+        // ");
+
+        // }
+
+        // while (true) {
+        // System.out.print("Period(Years): ");
+        // period = scanner.nextShort();
+        // if (period >= 1 && period <= 30) {
+        // int year_period = period * MONTHS_IN_YEAR;
+
+        // double mortgage_calculator = principal
+        // * (annual_interest_rate * Math.pow(1 + annual_interest_rate, year_period))
+        // / (Math.pow(1 + annual_interest_rate, year_period) - 1);
+        // NumberFormat currency = NumberFormat.getCurrencyInstance();
+        // String result = currency.format(mortgage_calculator);
+        // System.out.println(result);
+        // break;
+        // }
+        // System.out.println("Enter a value between 1 and 30");
+
+        // }
+
+        while (true) {
             System.out.print("Principal: ");
             principal = scanner.nextInt();
-            if(principal >= 1_000 && principal <= 1_000_000){
+            if (principal >= 1_000 && principal <= 1_000_000) {
                 break;
             }
             System.out.println("Enter a number between 1,000 and 1,000,000 ");
         }
 
-        while(true){
+        while (true) {
             System.out.print("Annual Interest Rate: ");
-            interest_rate = scanner.nextDouble();
-            if(interest_rate > 0 && interest_rate <= 30){
-                double interest_rate_percent = interest_rate / PERCENT;
-                annual_interest_rate = interest_rate_percent / MONTHS_IN_YEAR;
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30) {
                 break;
             }
             System.out.println("Enter a value greater than 0 and less than or equal 30 ");
 
         }
 
-        while(true){
+        while (true) {
             System.out.print("Period(Years): ");
-            period = scanner.nextShort();
-            if(period >= 1 && period <= 30){
-               int year_period = period * MONTHS_IN_YEAR;
-
-                double mortgage_calculator = principal * (annual_interest_rate * Math.pow(1 + annual_interest_rate , year_period)) / (Math.pow(1 + annual_interest_rate , year_period) - 1);
-                NumberFormat currency = NumberFormat.getCurrencyInstance();
-                String result = currency.format(mortgage_calculator);
-                System.out.println(result);
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
                 break;
             }
             System.out.println("Enter a value between 1 and 30");
 
         }
+
+        double mortgage_calculator = calculateMortgage(principal, annualInterest, years);
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(mortgage_calculator);
+        System.out.println(result);
+    }
+
+
+    // Extracting Methods
+    // create methods instead of coding all in the main function
+    public static double calculateMortgage(
+            int principal,
+            float annualInterest,
+            byte years) {
+        final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12;
+
+        float annual_interest_rate = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float year_period = years * MONTHS_IN_YEAR;
+
+        double mortgage_calculator = principal
+                * (annual_interest_rate * Math.pow(1 + annual_interest_rate, year_period))
+                / (Math.pow(1 + annual_interest_rate, year_period) - 1);
+
+        return mortgage_calculator;
     }
 }
